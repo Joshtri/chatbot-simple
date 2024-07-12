09b2142226bd077f5edd45917a738b5bd2a06d2c
Joshtri <stuffofyos1516@gmail.com>
2024-07-12 15:52:10 +0800

    push lts

--- nlpManager.js
+++ nlpManager.js
@@ -3,11 +3,24 @@ import { NlpManager } from 'node-nlp';
 const manager = new NlpManager({ languages: ['id'] }); // Mengatur bahasa yang digunakan adalah bahasa Indonesia ('id')
 
 // Menambahkan dokumen untuk data pelatihan dalam bahasa Indonesia
-manager.addDocument('id', 'siang', 'salam');
-manager.addDocument('id', 'malam', 'salam');
-manager.addDocument('id', 'halo', 'salam');
-manager.addDocument('id', 'yuhu', 'salam');
+manager.addDocument('id', 'ciao', 'salam');
+manager.addDocument('id', 'apa kabar', 'salam');
+manager.addDocument('id', 'selamat pagi', 'salam');
+manager.addDocument('id', 'selamat siang', 'salam');
+manager.addDocument('id', 'selamat malam', 'salam');
+manager.addDocument('id', 'yaa', 'salam');
 manager.addDocument('id', 'hey', 'salam');
+manager.addDocument('id', 'halo', 'salam');
+manager.addDocument('id', 'apa', 'salam');
+manager.addDocument('id', 'kabar', 'salam');
+manager.addDocument('id', 'sabar', 'salam');
+manager.addDocument('id', 'selamat', 'salam');
+manager.addDocument('id', 'selamat tinggal', 'salam');
+manager.addDocument('id', 'selamat jalan', 'salam');
+manager.addDocument('id', 'cuk', 'salam');
+manager.addDocument('id', 'kak', 'salam');
+manager.addDocument('id', 'dude', 'salam');
+manager.addDocument('id', 'bro', 'salam');
 manager.addDocument('id', 'hai bro', 'salam');
 manager.addDocument('id', 'apa kabar nih', 'tanya_kabar');
 manager.addDocument('id', 'gimana kabarnya', 'tanya_kabar');
@@ -26,6 +39,14 @@ manager.addDocument('id', 'tidak enak badan', 'respons_kabar_tidak_baik');
 // Menambahkan jawaban untuk intent tertentu
 manager.addAnswer('id', 'salam', 'Halo!');
 manager.addAnswer('id', 'salam', 'Hai!');
+manager.addAnswer('id', 'salam', 'Halo, apa kabar?');
+
+manager.addAnswer('id', 'salam', 'Hey, apa baru?');
+manager.addAnswer('id', 'salam', 'Yo, salam kenal!');
+manager.addAnswer('id', 'salam', 'Hai, apa mau?');
+manager.addAnswer('id', 'salam', 'Salam, apa kabar?');
+manager.addAnswer('id', 'salam', 'Hey, apa baru?');
+manager.addAnswer('id', 'salam', 'Yo, salam kenal!');
 
 manager.addAnswer('id', 'tanya_kabar', 'Kabar baik, terima kasih!');
 manager.addAnswer('id', 'tanya_kabar', 'Saya sedang baik, bagaimana dengan Anda?');
